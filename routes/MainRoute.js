fc547d753a24531c800c69eb39f81660af49ffe0
Mostafa Ebrahim <mostafa.ebrahim@hotmail.com>
2017-04-06 18:39:12 +0200

    Revert "submission"
    This reverts commit 3f4a7a3bf91b1f75e3f8803a31f80207c9aabf75.

--- routes/MainRoute.js
+++
@@ -1,8 +0,0 @@
-var placesCTRL = require('../controllers/placesCTRL');
-var express = require('express');
-var router =express.Router();
-
-router.get('/places',placesCTRL.viewPlaces);
-
-
-module.exports=router;
